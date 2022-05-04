ecd29a2ffb51e708be08914ab201fd8d1050f36e
JukisPastrana <104919363+JukisPastrana@users.noreply.github.com>
2022-05-04 10:49:08 -0500

    Lista
    Prueba

---
+++ main.py
@@ -0,0 +1,25 @@
+frutas=["Platano", "Manzana"]
+
+print(frutas)
+
+print(frutas[1])
+
+print(len(frutas))
+
+frutas.append("Pera ")
+
+print(frutas)
+
+frutas.append("Melon")
+
+print(frutas)
+
+frutas.sort()
+
+print(frutas)
+
+porcomprar=input("ingresa una fruta: ")
+
+frutas.append(porcomprar)
+
+print(frutas)
